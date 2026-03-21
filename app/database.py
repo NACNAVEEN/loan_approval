ad0cb6ddbc37cd5337121710e500d075e31bfd3f
Naveen <nacnaveen049@gmail.com>
2026-03-21 19:10:50 +0530

    DB changed

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/loan_approval_db"
+# DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/loan_approval_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
